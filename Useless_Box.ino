a5220bf47936a36876a6bf718bc9d671d0afe6f6
jakepichugin <66584925+jakepichugin@users.noreply.github.com>
2025-12-07 20:38:44 -0800

    Improved Code flow

--- Useless_Box.ino
+++ Useless_Box.ino
@@ -15,7 +15,7 @@ int switchState = 0;
 int flickNum = 0;
 
 Servo wall;
-bool wallHidden = true;
+bool wallWouldbeDisplayed = false;
 int wallStorageAngle = 60;
 int wallReachAngle = 0;
 int wallPin = 8;
@@ -40,6 +40,7 @@ bool flipped = false;
 bool detectedBeforeFlip = false;
 int detectedCounter = 0;
 bool handDetected = false;
+bool activateWall = false;
 
 void setup() {
   arm.attach(armPin);  
@@ -56,6 +57,9 @@ void setup() {
   
   pinMode(motorPin, OUTPUT);
   Serial.begin(9600);
+
+  arm.write(storageAngle);
+  wall.write(wallStorageAngle);
 }
 
 void loop() {
@@ -67,94 +71,162 @@ void loop() {
 
   if (detectingHand == true && flipped == false) {
     handDetected = true;
-    logger("detected");
+    // logger("detected");
     detectedCounter = 0;
   } 
 
   if (detectingHand == false && detectedCounter > 75 && handDetected == true) {
     handDetected = false;
-    logger("no longer detected");
+    // logger("no longer detected");
     detectedCounter = 0;
   } else {
     detectedCounter++;
   }
 
-  
-  
+  // if (checkSwitch() == true) { //switch was flipped
+  //   // logger("flip");
+  //   flip(flickNum);
+  // } else if (checkSwitch() == false){ //switch in correct position
+  //   logger("storing");
+  //   storeArm(1);
+  // }
+
   if (switchState == OFF){ //switch in the prefered position
     flipped = false;
-    moveWall();
     if (prevState == ON) {
       prevState = OFF;
-      arm.write(storageAngle);
-      
+      storeArm(1);
+      return false;
     }
     
-  } else { // switch not in the perfered position
+  } else if (switchState == ON){ // switch not in the perfered position
     flipped = true;
     if (prevState == OFF) {
-      reachAction(flickNum);
-      flickNum++;
       prevState = ON;
-      if (handDetected == false){
-        
-        move();
-      } 
-      // Serial.print("flicked: ");
-      // Serial.println(flickNum);
+      // if (handDetected == false){
+      //   move();
+      // } 
+      flip(flickNum);
+      return true;
     }
-    digitalWrite(13, LOW); // turning on built in light
-
-    
   }
+
+  if (activateWall == true) {
+    moveWall();
+  }
+  
+  
   delay(15);
 }
 
+void flip(int attempt){
+  flickNum++;
+  // Serial.println(attempt);
+  if(attempt == 0) {
+    delay(1000);
+    arm.write(reachAngle);
+  } else if(attempt > 0 && attempt < 6) {
+      reachArm(false); // [][][][][][][][][][][][][][][][]change to true for final
+  } else if(attempt > 5 && attempt < 16){
+      reachArm(false);
+      activateWall = true;
+    
+  } else {
+    //arm.write(reachAngle);
+  }
+}
+
+void storeArm(int c) {
+  switch (c) {
+    case 1: //normal
+      arm.write(storageAngle);
+      break;
+    case 2:
+      arm.write(peekAngle);
+      delay(1000);
+      arm.write(storageAngle);
+      break;
+  }
+}
+
+
+void reachArm(bool includePeek){
+  switch (random(1, 4)) {
+    case 1:
+    case 2: 
+      arm.write(reachAngle);
+      break;
+    case 3:
+      if (includePeek) {
+        arm.write(peekAngle);
+        delay(1500);
+        arm.write(reachAngle);
+      } else {
+        arm.write(reachAngle);
+      }
+  }
+}
+
+bool checkSwitch() {
+  if (switchState == OFF){ //switch in the prefered position
+    flipped = false;
+    if (prevState == ON) {
+      prevState = OFF;
+      storeArm(1);
+      return false;
+    }
+    
+  } else if (switchState == ON){ // switch not in the perfered position
+    flipped = true;
+    if (prevState == OFF) {
+      prevState = ON;
+      // if (handDetected == false){
+      //   move();
+      // } 
+      flip(flickNum);
+      logger("true");
+      return true;
+    }
+  }
+}
 
 int moveCounter = 0;
 void move() {
-  
   while (moveCounter < 30) {
     digitalWrite(motorPin, HIGH);
     logger("moving");
     moveCounter++;
   } 
-
   logger("stopping");
   digitalWrite(motorPin, LOW);
   
   moveCounter = 0;
 
 }
-  
-  
-  
-
-void reachAction(int attempt){
-  if(attempt == 0) {
-    delay(1000);
-    arm.write(reachAngle);
-  } else if(attempt < 20) {
-      arm.write(reachAngle);
-  } else {
-    arm.write(reachAngle);
-  }
-}
-
+   
 int counter = 0;
+int wallWouldBeDisplayedCounter = 0;
+int holdWallDisplayCounter = random(1, 4);
 void moveWall(){
+  
   counter++;
   if (detectingHand && distance < 15 && switchState == OFF) {
-    if (wallHidden == true){
-      wall.write(wallReachAngle);
-      wallHidden = false;
+    if (wallWouldbeDisplayed == false){
+      wallWouldBeDisplayedCounter++;
+        
+      if(wallWouldBeDisplayedCounter > holdWallDisplayCounter) {
+        logger("wall");  
+        wall.write(wallReachAngle);
+        wallWouldBeDisplayedCounter = 0;
+        holdWallDisplayCounter = random(1, 4);
+      }
+      wallWouldbeDisplayed = true;
       counter = 0;
     }
-  } else {
-    if (wallHidden == false && counter > 25) {
-      wall.write(wallStorageAngle);
-      wallHidden = true;
-    }
+  } else if (wallWouldbeDisplayed == true && counter > 50) {
+    wall.write(wallStorageAngle); //will be called extra times
+    wallWouldbeDisplayed = false;
+    logger("hiddingwall");
   }
 }
 
